f89ec872805c6f3619eafc6f2e46b16eb38b0d7d
aldimas07 <aldimasjaya.07@gmail.com>
2023-06-04 23:06:50 +0700

    add auth.js, implemented middleware in routes.js

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      statusCode: 401,
+      error: 'Unauthorized',
+    });
+  }
+  jwt.verify(token, 'your-secret-key', (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        statusCode: 403,
+        error: 'Forbidden',
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const crudRouter = express.Router();
 const authRouter = express.Router();
 const userController = require('../controllers/userController');
+const needAuthenticate = require('../middlewares/auth');
 
-crudRouter.get('/', userController.getAllUsers);
-crudRouter.get('/:email', userController.getUserByEmail);
+crudRouter.get('/', needAuthenticate, userController.getAllUsers);
+crudRouter.get('/:email', needAuthenticate, userController.getUserByEmail);
 crudRouter.get('/id/:id', userController.getUserByID);
 crudRouter.put('/:email', userController.updateUserByEmail);
-crudRouter.delete('/:id', userController.deleteUserByID);
+crudRouter.delete('/:id', needAuthenticate, userController.deleteUserByID);
 
 authRouter.post('/register', userController.createNewUser);
 authRouter.post('/login', userController.login);
