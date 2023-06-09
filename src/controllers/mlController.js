9e90e8a77830684d55fc563606c34e44065ba32e
aldimas07 <aldimasjaya.07@gmail.com>
2023-06-09 13:35:44 +0700

    some changes

--- src/controllers/mlController.js
+++ src/controllers/mlController.js
@@ -9,19 +9,15 @@ class MLController {
       const canvas = createCanvas(256, 256);
       const ctx = canvas.getContext('2d');
       ctx.drawImage(image, 0, 0, 256, 256);
-
       const tensor = tf.browser.fromPixels(canvas).expandDims();
       const predictions = await predict(tensor);
       const probability = predictions[0];
-
       const result = {
         statusCode: 200,
         probability: probability.toFixed(2),
         prediction: probability >= 0.5 ? "This is not categorized as e-waste, you can't send to collector." : 'This is an e-waste, you can send to collector!',
       };
-
       res.json(result);
-
       console.log('Prediction:', result);
     } catch (error) {
       console.error('Prediction error:', error);
