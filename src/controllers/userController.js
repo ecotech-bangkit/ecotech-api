83a56a96f0d3a6dc4dea55f62d8a459a14fd778e
losbanditos <aldimasjaya.07@gmail.com>
2024-07-20 13:12:04 +0700

    feat(getOrderEwasteByKolektorIdAndStatusMenunggu): implement caching to reduce response time

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const GCS_BUCKET_NAME = process.env.GCS_BUCKET_NAME;
 // const { Storage } = require('@google-cloud/storage');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 600 });
 const { uploadImageToS3 } = require('../modules/imgUploadS3')
 const { generateTokenPair } = require('../middlewares/auth');
 const { data } = require('@tensorflow/tfjs');
@@ -192,6 +194,7 @@ const getOrderEwasteByStatus = async (req, res) => {
 }
 
 const getOrderEwasteByKolektorIdAndStatusMenunggu = async (req, res) => {
+  
   const { kolektor_id } = req.query
 
   if (!kolektor_id) {
@@ -201,30 +204,33 @@ const getOrderEwasteByKolektorIdAndStatusMenunggu = async (req, res) => {
     });
   }
 
-  try {
-    const [data] = await userModel.getOrderEwasteByKolektorIdAndStatusMenunggu(kolektor_id)
-    if (!data) {
-      res.status(404).json({
-        statusCode: 404,
-        message: 'Orders not found'
+  const cacheKey = `orders_${kolektor_id}`;
+  let data = cache.get(cacheKey);
+  if(!data){
+    try {
+      [data] = await userModel.getOrderEwasteByKolektorIdAndStatusMenunggu(kolektor_id)
+      if (!data) {
+        res.status(404).json({
+          statusCode: 404,
+          message: 'Orders not found'
+        })
+        return
+      }
+      cache.set(cacheKey, data);
+    } catch (error) {
+      console.error('Error fetching orders:', error);
+      res.status(500).json({
+        statusCode: 500,
+        message: 'Internal Server Error',
+        errorMessage: error.sqlMessage
       })
-      return
     }
-
-    res.status(200).json({
-      statusCode: 200,
-      message: 'Orders retrieved successfully',
-      data: data
-    })
-
-  } catch (error) {
-    console.error('Error fetching orders:', error);
-    res.status(500).json({
-      statusCode: 500,
-      message: 'Internal Server Error',
-      errorMessage: error.sqlMessage
-    })
   }
+  res.status(200).json({
+    statusCode: 200,
+    message: 'Orders retrieved successfully',
+    data: data
+  })
 }
 
 const getAllOrderEwaste = async (req, res) => {
